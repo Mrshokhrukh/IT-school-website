90c6bba30fdf2ed33856f1ae2abb549aad498a7a
MrShokhrukh <shokhrukhdot@gmail.com>
2023-02-27 18:15:40 +0500

    header part(1)

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoMdLogIn } from "react-icons/io";
+import logo from "../../images/rakhmonov.png";
 import "./header.style.scss";
 const Header = () => {
   return (
     <div className="head">
       <div className="header">
-        <div className="logo">
-          <span>logo</span>
+        <div className="head-rt">
+          <span className="logo">
+            <img src={logo} alt="" />
+          </span>
         </div>
-        
+        <div className="head-lf">
+          <Link to='/courses' className="link-to-courses">Kurslarimiz</Link>
+          <div className="login">
+            <span className="login-icon">
+              <IoMdLogIn />
+            </span>
+            <span>kirish</span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/layout.js
+++ client/src/layout.js
@@ -7,7 +7,7 @@ const Layout = () => {
     <div>
       <Header />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
