fb21b6f6d469a5c850758afb3127dd3fef27d0e6
MrShokhrukh <shokhrukhdot@gmail.com>
2023-03-05 05:38:59 +0500

    yet process

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { IoMdLogIn } from "react-icons/io";
 import logo from "../../images/rakhmonov.png";
 import "./header.style.scss";
 const Header = () => {
+  const [width, setWidth] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWidth(window.innerWidth);
+      if (width < 768) {
+        setIsOpen(true);
+      } else {
+        setIsOpen(false);
+      }
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, [width]);
+
   return (
     <div className="head">
       <div className="container">
@@ -13,20 +30,34 @@ const Header = () => {
               <img src={logo} alt="" />
             </span>
           </div>
-          <div className="head-rt">
-            <Link to="/" className="link-to-courses">
-              bootcamp
-            </Link>
-            <Link to="/courses" className="link-to-courses">
-              Kurslarimiz
-            </Link>
-            <div className="login">
-              <span className="login-icon">
-                <IoMdLogIn />
-              </span>
-              <span>kirish</span>
+         
+            
+         
+            <div className="head-rt">
+              <Link to="/" className="link-to-courses">
+                bootcamp
+              </Link>
+              <Link to="/courses" className="link-to-courses">
+                Kurslarimiz
+              </Link>
+              <div className="login">
+                <span className="login-icon">
+                  <IoMdLogIn />
+                </span>
+                <span>kirish</span>
+              </div>
             </div>
-          </div>
+
+{/* 
+            <div className="sidebar">
+              <div className="hamburger">
+                <div className="hamburger__container">
+                  <div className="hamburger__inner"></div>
+                  <div className="hamburger__hidden"></div>
+                </div>
+              </div>
+            </div> */}
+          
         </div>
       </div>
     </div>
---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './sidebar.scss'
+const Sidebar = () => {
+  return (
+    <div className='header_sidebar'>
+        
+
+
+    </div>
+  )
+}
+
+export default Sidebar
